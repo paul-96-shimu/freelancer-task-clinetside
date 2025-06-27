02f40694f1b1b4db35f309da67b11fd9536d66bf
shimu paul <shimupaul96@gmail.com>
2025-06-27 10:44:35 +0600

    added error page

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div className="h-screen flex flex-col justify-center items-center text-center">
-            <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
-            <p className="text-xl mb-6">Oops! The page you are looking for does not exist.</p>
+        <div className="h-screen flex flex-col justify-center items-center text-center bg-gray-50 px-4">
+            {/* <img
+        src={errorImage}
+        alt="404 Not Found"
+        className="max-w-xs md:max-w-md mb-6 animate-bounce"
+      /> */}
+
+            <img src="https://i.ibb.co/HTQ34xML/error-page.jpg" alt=""
+                className="max-w-xs md:max-w-md mb-6 animate-bounce"
+            />
+            <h1 className="text-5xl font-extrabold text-red-500 mb-2">404</h1>
+            <p className="text-xl text-gray-700 mb-4">
+                Oops! The page you are looking for doesn’t exist.
+            </p>
             <Link to="/" className="btn btn-primary">Go to Home</Link>
         </div>
     );
